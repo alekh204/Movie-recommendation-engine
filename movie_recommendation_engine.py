52457d5127e113fca8f95b7ebb7486d2eee475b2
alekh <alekhintpp@gmail.com>
2019-08-21 22:15:43 +0530

    first commit

---
+++ movie_recommendation_engine.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+###### helper functions. Use them when needed #######
+def get_title_from_index(index):
+	return df[df.index == index]["title"].values[0]
+
+def get_index_from_title(title):
+	return df[df.title == title]["index"].values[0]
+##################################################
+
+##Step 1: Read CSV File
+df = pd.read_csv("imdb.csv",error_bad_lines=False)
+print (df.columns)
+##Step 2: Select Features
+
+features = ['title','imdbRating','ratingCount', 'duration', 'year']
+##Step 3: Create a column in DF which combines all selected features
+for feature in features:
+	df[feature] = df[feature].fillna('')
+
+def combine_features(row):
+	try:
+		return row['title'] +" "+row['imdbRating']+" "+row["ratingCount"]+" "+row["duration"]+" "+row["year"]
+	except:
+		print ("Error:", row)	
+df["combined_features"] = df.apply(combine_features,axis=1)
+
+df["combined_features"].head()
+
+print ("combined_features") 
+cv = CountVectorizer()
+
+count_matrix = cv.fit_transform(df["combined_features"])
+
+##Step 5: Compute the Cosine Similarity based on the count_matrix
+cosine_sim = cosine_similarity(count_matrix) 
+movie_user_likes = "Kalyug (2005)"
+
+## Step 6: Get index of this movie from its title
+movie_index = get_index_from_title(movie_user_likes)
+
+similar_movies =  list(enumerate(cosine_sim[movie_index]))
+
+## Step 7: Get a list of similar movies in descending order of similarity score
+sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)
+
+## Step 8: Print titles of first 50 movies
+i=0
+for element in sorted_similar_movies:
+		print (get_title_from_index(element[0]))
+		i=i+1
+		if i>50:
+			break
